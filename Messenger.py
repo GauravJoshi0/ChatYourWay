2058d9d117d0e5679050f9d6e778e174c5bc99ac
Gaurav <codingmeanlogic008gmail.com>
2023-12-13 22:48:07 +0545

    prettify

--- Messenger.py
+++ Messenger.py
@@ -1,10 +1,15 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.core.text import LabelBase
+from kivymd.uix.button import MDIconButton
 from kivy import Config
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.metrics import dp
 
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
-LabelBase.register(name="nav_rail", fn_regular="fonts\\Nasalization-rg.ttf")
+LabelBase.register(name="nav_rail", fn_regular="fonts\\CourierPrime-Regular.ttf")
+LabelBase.register(name="message", fn_regular="fonts\\Poppins-Regular.ttf")
+LabelBase.register(name="test", fn_regular="fonts\\Poppins-Black.ttf")
 
 class Messenger(MDApp):
     def build(self):
