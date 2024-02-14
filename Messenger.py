433b3c61c0224817aaf501a60cd02403bea3966f
Gaurav <codingmeanlogic008gmail.com>
2024-02-14 23:05:58 +0545

    User diallocation fixed

--- Messenger.py
+++ Messenger.py
@@ -1,8 +1,10 @@
 from kivymd.app import MDApp
 from kivymd.uix.button import MDIconButton
+from kivymd.uix.label import MDLabel
 from kivy import Config
 from kivy.lang import Builder
 from kivy.core.text import LabelBase
+from kivy.uix.screenmanager import Screen, ScreenManager
 
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
@@ -10,30 +12,60 @@ LabelBase.register(name="nav_rail", fn_regular="fonts\\CourierPrime-Regular.ttf"
 LabelBase.register(name="nasalization", fn_regular="fonts\\nasalization-rg.otf")
 LabelBase.register(name="message", fn_regular="fonts\\Poppins-Regular.ttf")
 
-class Messenger(MDApp):
+class User(MDLabel):
+    ...
+
+class Response(MDLabel):
+    ...
+
+class SettingWindow(Screen):
+    ...
+
+class ChatWindow(Screen):
+    def send(self, object):
+        message=self.manager.get_screen("chatwindow").ids.text_field.text
+        layout=self.manager.get_screen("chatwindow").ids.layout
+        if (len(message))<=5:
+            lblsize=0.1
+        elif (len(message))<=14:
+            lblsize=0.19
+        elif (len(message))<=20:
+            lblsize=0.27
+        else:
+            lblsize=0.4
+        layout.add_widget(User(text=message, valign="center", size_hint_x=lblsize))
+        # self.manager.get_screen("chatwindow").ids.text_field.text=""
+        
     def on_textfield_focus(self):
-        message_box=self.root.ids.text_field
+        message_box=self.manager.get_screen("chatwindow").ids.text_field
         if message_box.focus:
-            widget_rm=self.root.ids.field_layout.remove_widget
-            widget_rm(self.root.ids.mike)
-            widget_rm(self.root.ids.attach)
+            widget_rm=self.manager.get_screen("chatwindow").ids.field_layout.remove_widget
+            widget_rm(self.manager.get_screen("chatwindow").ids.mike)
+            widget_rm(self.manager.get_screen("chatwindow").ids.attach)
 
             global send
             send=MDIconButton(
                 icon="images\send.png",
                 ripple_scale=0,
-                pos_hint={"center_y": .65}
+                pos_hint={"center_y": .5, "right": 1},
+                on_press=self.send
 
             )
-            self.root.ids.layout_main1.add_widget(send)
+            self.manager.get_screen("chatwindow").ids.field_layout.add_widget(send)
             
 
         elif not message_box.focus:
-            widget_add=self.root.ids.field_layout.add_widget
-            widget_add(self.root.ids.mike)
-            widget_add(self.root.ids.attach)
+            widget_add=self.manager.get_screen("chatwindow").ids.field_layout.add_widget
+            widget_add(self.manager.get_screen("chatwindow").ids.mike)
+            widget_add(self.manager.get_screen("chatwindow").ids.attach)
 
-            self.root.ids.layout_main1.remove_widget(send)
+            self.manager.get_screen("chatwindow").ids.field_layout.remove_widget(send)
+
+class WindowManager(ScreenManager):
+    ...
+
+class Messenger(MDApp):
+
 
     def build(self):
         self.icon="images\messenger.png"
